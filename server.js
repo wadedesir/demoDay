176bf899c922bcb18fd1a21baefbdc03821548bf
Wade Desir <wededesir@gmail.com>
2021-03-27 18:25:21 -0400

    basic login % sign up

--- server.js
+++ server.js
@@ -25,7 +25,16 @@ let db
 
 // configuration ===============================================================
 app.use(bodyParser.urlencoded({ extended: true }))
-setupRoutes(app)
+
+mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  // require('./app/routes.js')(app, passport, db)
+  setupRoutes(app, passport, db);
+}); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
@@ -33,12 +42,19 @@ app.use(express.json()); // get information from html forms
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-// var distDir = __dirname + "/dist/";
-// app.use(express.static(distDir));
-
 app.set('view engine', 'ejs'); // set up ejs for templating
 
+// required for passport
+app.use(session({
+    secret: 'rcbootcamp2021a', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
+
+
 // launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
