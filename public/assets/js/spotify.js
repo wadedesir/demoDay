60d38f0e81489b691a87f071f9c1bed5d990839a
Wade Desir <wededesir@gmail.com>
2021-04-21 16:39:33 -0400

    player working

---
+++ public/assets/js/spotify.js
@@ -0,0 +1,57 @@
+// window.onSpotifyWebPlaybackSDKReady = () => {
+//     // You can now initialize Spotify.Player and use the SDK
+//   };
+async function getToken(){
+    const fetchToken = await fetch('/token');
+    console.log(fetchToken)
+    const token = await fetchToken.json();
+    return token
+}
+// const deviceId
+const token = getToken()
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+    
+    const player = new Spotify.Player({
+      name: 'Duanote Player',
+      getOAuthToken: cb => { cb(token); }
+    });
+  
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+  
+    // Playback status updates
+    player.addListener('player_state_changed', state => { console.log(state); });
+  
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+      deviceId = device_id
+      fetch('/initializePlayer');
+        
+    });
+  
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+  
+    // Connect to the player!
+    player.connect();
+  };
+
+document.querySelector('.guided').addEventListener('click', playMedia)
+document.querySelector('.freePlay').addEventListener('click', pauseMedia)
+
+async function playMedia(){
+  const play = await fetch('/play');
+  console.log(play)
+}
+
+async function pauseMedia(){
+  const play = await fetch('/pause');
+  console.log(play)
+}
