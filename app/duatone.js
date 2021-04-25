004ddce25371d042b708a34bb9283bb5ddccbfc9
Wade Desir <wededesir@gmail.com>
2021-04-24 22:09:49 -0400

    fix links

--- app/duatone.js
+++ app/duatone.js
@@ -7,12 +7,12 @@ function duatoneSetup(SpotifyWebApi){ //generate duatonePlayer and User objects
     const duatonePlayer = new SpotifyWebApi({
         clientId: '1f3c90c77fce4b60bd9e18d35175bd86',
         clientSecret: '8758a46abe0a4ff2abb77245a9b64c2d',
-        redirectUri: 'https://duatone.herokuapp.com/onboard'
+        redirectUri: 'https://duatone.herokuapp.com/connect'
         });
       
         //generate spotify auth url for user
         const scopes = ['user-read-private', 'user-read-email', 'user-read-recently-played', 'user-top-read', 'user-modify-playback-state', 'user-follow-read', 'user-library-modify', 'user-library-read', 'streaming', 'user-read-playback-state', 'user-read-currently-playing', 'app-remote-control'],
-        redirectUri = 'https://duatone.herokuapp.com/onboard',
+        redirectUri = 'https://duatone.herokuapp.com/connect',
         clientId = '1f3c90c77fce4b60bd9e18d35175bd86',
         state = 'duatone';
       
